a572649b3c615229dfde5febfe2087690d661dc7
teja9398 <dharmateja9398@gmail.com>
2023-09-24 17:44:20 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        roboto:['Major Mono Display ,chakra petch']
+      }
+    },
+  },
+  plugins: [],
+}
+
